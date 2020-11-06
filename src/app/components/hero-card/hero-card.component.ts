d57000b9519b80751fbdff7378604bc194b508ab
Adam Callanan <a.callanan@sbcglobal.net>
2020-11-06 00:11:49 -0600

    last steps

--- src/app/components/hero-card/hero-card.component.ts
+++ src/app/components/hero-card/hero-card.component.ts
@@ -13,12 +13,12 @@ export class HeroCardComponent {
     public showFullMessage = false;
 
     public get heroTitle(): string {
-        return `${this.hero.name} Lvl. ${this.hero.level}`;
+        return `${this.hero.name} ~ Lvl. ${this.hero.level} ~ ${this.hero.race.toLowerCase()} ~ ${this.hero.alignment.toLowerCase()}`;
     }
 
     public get heroSubtitle(): string {
-        const distanceText = BooleanHelper.hasValue(this.hero.distanceText) ? `${this.hero.distanceText} |` : "";
-        return `${this.hero.hpText} | ${distanceText} ${this.hero.expText} | ${this.hero.age} updates`;
+        const distanceText = BooleanHelper.hasValue(this.hero.distanceText) ? `${this.hero.distanceText} ~` : "";
+        return `${this.hero.hpText} ~ ${distanceText} ${this.hero.expText} ~ ${this.hero.age} updates`;
     }
 
     public get hasHero(): boolean {
